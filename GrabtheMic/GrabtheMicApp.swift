a9eccee199662a2cfa2cff8a75ec06e7f8c7b5cd
k1chen <chenaun@bc.edu>
2025-04-04 15:01:56 -0400

    Initial Commit

---
+++ GrabtheMic/GrabtheMicApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabtheMicApp.swift
+//  GrabtheMic
+//
+//  Created by Kenneth Chen on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GrabtheMicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
